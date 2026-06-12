768b36fd958e2390abc9609ed3aa753cd07e7e00
kundan <chouhankundan60023@gmail.com>
2026-06-12 14:41:59 +0530

    Create admin authentication module

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 require("./src/models/User");
 require("./src/models/Agent");
+require("./src/models/Admin");
 
 const connectDB = require("./src/config/db");
 const testRoutes = require("./src/routes/testRoutes");
@@ -11,6 +12,7 @@ const authRoutes = require("./src/routes/authRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 const pickupRoutes = require("./src/routes/pickupRoutes");
 const agentRoutes = require("./src/routes/agentRoutes");
+const adminRoutes = require("./src/routes/adminRoutes");
 
 const app = express();
 
@@ -24,6 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/pickup", pickupRoutes);
 app.use("/api/agent", agentRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.send("Smart Scrap Pickup API Running");
