09252ff98a636e7b0e7251a3d71d65f74496ef3a
kundan <chouhankundan60023@gmail.com>
2026-06-12 17:21:17 +0530

    Implement user login page and JWT storage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserDashboard from "./pages/UserDashboard";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/UserDashboard.jsx
@@ -0,0 +1,13 @@
+function UserDashboard() {
+  return (
+    <div>
+      <h1>User Dashboard</h1>
+
+      <h3>
+        Welcome to Smart Scrap Pickup
+      </h3>
+    </div>
+  );
+}
+
+export default UserDashboard;
